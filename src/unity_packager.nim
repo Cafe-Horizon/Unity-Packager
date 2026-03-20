0b989773b3d5a0e16fa98b9c05eae5e53a987640
malken21 <77374813+malken21@users.noreply.github.com>
2026-03-20 20:13:49 +0900

    Update unity_packager.nim

--- src/unity_packager.nim
+++ src/unity_packager.nim
@@ -1,6 +1,14 @@
 import std/[os, strutils, times, parseopt, tables, sets]
 import zippy, zippy/tarballs
 
+type
+  PackagerOptions = object
+    inputDir: string
+    outputFile: string
+    includeDeps: bool
+
+  AssetPaths = tuple[meta: string, asset: string]
+
 proc getGuid(metaPath: string): string =
   try:
     let f = open(metaPath)
@@ -60,43 +68,32 @@ proc buildGuidMap(projectRoot: string): Table[string, string] =
           res[guid] = path[0 .. ^6]
   return res
 
-proc main() =
-  var inputDir = ""
-  var outputFile = ""
-  var includeDeps = false
-  
+proc parseCliArgs(): PackagerOptions =
+  var opts = PackagerOptions(includeDeps: false)
   for kind, key, val in getopt():
     case kind
     of cmdArgument:
-      if inputDir == "": inputDir = key
-      elif outputFile == "": outputFile = key
+      if opts.inputDir == "": opts.inputDir = key
+      elif opts.outputFile == "": opts.outputFile = key
     of cmdLongOption, cmdShortOption:
       if key == "dependencies" or key == "d":
         if val == "" or val.toLowerAscii() == "true":
-          includeDeps = true
+          opts.includeDeps = true
         elif val.toLowerAscii() == "false":
-          includeDeps = false
+          opts.includeDeps = false
     of cmdEnd: discard
 
-  if inputDir == "" or outputFile == "":
+  if opts.inputDir == "" or opts.outputFile == "":
     echo "Usage: packager [options] <inputDir> <outputFile.unitypackage>"
     echo "Options:"
     echo "  -d, --dependencies[=true|false]  Include dependencies (default: false)"
     quit(1)
-    
-  inputDir = absolutePath(inputDir)
-  outputFile = absolutePath(outputFile)
-  
-  let tempArchDir = getTempDir() / ("unitypackager_" & $getTime().toUnix())
-  let tempTarFile = tempArchDir & ".tar"
-  defer:
-    if fileExists(tempTarFile): removeFile(tempTarFile)
-    if dirExists(tempArchDir): removeDir(tempArchDir)
-  createDir(tempArchDir)
-  
-  var targetGuids = initHashSet[string]()
-  var guidToPaths = initTable[string, tuple[meta: string, asset: string]]()
 
+  opts.inputDir = absolutePath(opts.inputDir)
+  opts.outputFile = absolutePath(opts.outputFile)
+  return opts
+
+proc collectTargetAssets(inputDir: string, targetGuids: var HashSet[string], guidToPaths: var Table[string, AssetPaths]) =
   var dirsToVisit = @[inputDir]
   while dirsToVisit.len > 0:
     let currentDir = dirsToVisit.pop()
@@ -109,38 +106,38 @@ proc main() =
           let assetPath = path[0 .. ^6]
           if dirExists(assetPath) or fileExists(assetPath):
             targetGuids.incl(guid)
-            guidToPaths[guid] = (path, assetPath)
+            guidToPaths[guid] = (meta: path, asset: assetPath)
 
-  if includeDeps:
-    let projRoot = findProjectRoot(inputDir)
-    let globalGuidMap = buildGuidMap(projRoot)
-    var queue = newSeq[string]()
-    for g in targetGuids: queue.add(g)
-    var processed = initHashSet[string]()
+proc resolveDependencies(inputDir: string, targetGuids: var HashSet[string], guidToPaths: var Table[string, AssetPaths]) =
+  let projRoot = findProjectRoot(inputDir)
+  let globalGuidMap = buildGuidMap(projRoot)
+  var queue = newSeq[string]()
+  for g in targetGuids: queue.add(g)
+  var processed = initHashSet[string]()
+  
+  while queue.len > 0:
+    let currGuid = queue.pop()
+    if processed.contains(currGuid): continue
+    processed.incl(currGuid)
     
-    while queue.len > 0:
-      let currGuid = queue.pop()
-      if processed.contains(currGuid): continue
-      processed.incl(currGuid)
-      
-      let paths = guidToPaths.getOrDefault(currGuid, (meta: "", asset: ""))
-      let assetPath = paths.asset
-      if assetPath != "" and fileExists(assetPath):
-        let ext = splitFile(assetPath).ext.toLowerAscii()
-        if ext in [".prefab", ".mat", ".unity", ".controller", ".anim", ".asset"]:
-          let deps = extractDependencies(assetPath)
-          for dGuid in deps:
-            if not targetGuids.contains(dGuid) and globalGuidMap.hasKey(dGuid):
-              targetGuids.incl(dGuid)
-              let dAssetPath = globalGuidMap[dGuid]
-              guidToPaths[dGuid] = (dAssetPath & ".meta", dAssetPath)
-              queue.add(dGuid)
+    let paths = guidToPaths.getOrDefault(currGuid, (meta: "", asset: ""))
+    let assetPath = paths.asset
+    if assetPath != "" and fileExists(assetPath):
+      let ext = splitFile(assetPath).ext.toLowerAscii()
+      if ext in [".prefab", ".mat", ".unity", ".controller", ".anim", ".asset"]:
+        let deps = extractDependencies(assetPath)
+        for dGuid in deps:
+          if not targetGuids.contains(dGuid) and globalGuidMap.hasKey(dGuid):
+            targetGuids.incl(dGuid)
+            let dAssetPath = globalGuidMap[dGuid]
+            guidToPaths[dGuid] = (meta: dAssetPath & ".meta", asset: dAssetPath)
+            queue.add(dGuid)
 
+proc preparePackageTempDir(inputDir: string, tempArchDir: string, targetGuids: HashSet[string], guidToPaths: Table[string, AssetPaths]) =
   for guid in targetGuids:
     let paths = guidToPaths[guid]
     let metaPath = paths.meta
     let assetPath = paths.asset
-    let isDir = dirExists(assetPath)
     let isFile = fileExists(assetPath)
     
     let guidDir = tempArchDir / guid
@@ -162,17 +159,7 @@ proc main() =
       
     writeFile(guidDir / "pathname", finalPath & "\n")
 
-  if targetGuids.len == 0:
-    echo "No assets found to package."
-    quit(0)
-
-  let tarball = Tarball()
-  for guid in targetGuids:
-    tarball.addDir(tempArchDir / guid)
-  tarball.writeTarball(tempTarFile)
-  
-  let tarData = readFile(tempTarFile)
-  let gzData = compress(tarData, BestCompression, dfGzip)
+proc overrideGzipOriginalName(gzData: string, newName: string): string =
   var finalData = gzData[0..2]
   var flg = cast[uint8](gzData[3])
   
@@ -189,11 +176,48 @@ proc main() =
   if (flg and 0x02) != 0:
     pos += 2
     
-  flg = flg or 0x08
+  flg = flg or 0x08 # Ensure FNAME flag is set
   finalData.add cast[char](flg)
   finalData.add gzData[4..9]
-  finalData.add "archtemp.tar\0"
+  finalData.add newName & "\0"
   finalData.add gzData[pos..^1]
-  writeFile(outputFile, finalData)
+  return finalData
 
-main()
+proc executePackaging*(opts: PackagerOptions) =
+  let tempArchDir = getTempDir() / ("unitypackager_" & $getTime().toUnix())
+  let tempTarFile = tempArchDir & ".tar"
+  defer:
+    if fileExists(tempTarFile): removeFile(tempTarFile)
+    if dirExists(tempArchDir): removeDir(tempArchDir)
+  createDir(tempArchDir)
+  
+  var targetGuids = initHashSet[string]()
+  var guidToPaths = initTable[string, AssetPaths]()
+
+  collectTargetAssets(opts.inputDir, targetGuids, guidToPaths)
+
+  if opts.includeDeps:
+    resolveDependencies(opts.inputDir, targetGuids, guidToPaths)
+
+  if targetGuids.len == 0:
+    echo "No assets found to package."
+    return
+
+  preparePackageTempDir(opts.inputDir, tempArchDir, targetGuids, guidToPaths)
+
+  let tarball = Tarball()
+  for guid in targetGuids:
+    tarball.addDir(tempArchDir / guid)
+  tarball.writeTarball(tempTarFile)
+  
+  let tarData = readFile(tempTarFile)
+  let gzData = compress(tarData, BestCompression, dfGzip)
+  let finalData = overrideGzipOriginalName(gzData, "archtemp.tar")
+  writeFile(opts.outputFile, finalData)
+
+proc main() =
+  let opts = parseCliArgs()
+  executePackaging(opts)
+
+when isMainModule:
+  main()
